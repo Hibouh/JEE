c8b6c01dc57e658899bb653afb4741c3aac491b9
Hibouh <dan.le@etu.univ-amu.fr>
2019-04-23 19:11:39 +0200

    Update

---
+++ JPA/src/myapp/dao/IPersonDao.java
@@ -0,0 +1,25 @@
+package myapp.dao;
+
+import java.util.Collection;
+
+import myapp.model.Group;
+import myapp.model.Person;
+
+public interface IPersonDao {
+
+	// récupérer les groupes
+	Collection<Group> findAllGroups();
+
+	// récupérer les personnes
+	Collection<Person> findAllPersons(long groupId);
+
+	// lire une personne
+	Person findPerson(long id);
+
+	// modification ou ajout d'une nouvelle personne
+	void savePerson(Person p);
+
+	// modification ou ajout d'une nouvelle personne
+	void saveGroup(Group g);
+
+}
